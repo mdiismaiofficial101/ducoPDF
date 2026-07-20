1a7387cf8a292ba43bce7994cf413ce9e71b0482
mdismail <mdismail@docupdf.com>
2026-07-20 17:10:20 +0600

    Add Monetag in-page (zone 11353390) + in-page push (zone 11353370) scripts to site body

--- app/layout.tsx
+++ app/layout.tsx
@@ -157,6 +157,14 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <main className="flex-grow">
           {children}
         </main>
+
+        <Script id="monetag-inpage" strategy="afterInteractive">
+          {(function(s){s.dataset.zone='11353390',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))}
+        </Script>
+        <Script id="monetag-inpage-push" strategy="afterInteractive">
+          {(function(s){s.dataset.zone='11353370',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))}
+        </Script>
+
         <ShowFooter />
         <UserConsent />
         <Script id="register-sw" strategy="afterInteractive">
